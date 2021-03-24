575e3e77134c69f1db3659833450f942fdcfe595
Beatriz Marques Faria de Barros <71844667+z1rta@users.noreply.github.com>
2021-03-24 19:54:03 -0300

    Create 04.py

---
+++ Python/Exercícios/04.py
@@ -0,0 +1,6 @@
+ano = 1996
+nome = "Beatriz Marques"
+saldo = 56.99
+print("O tipo da variável é {}".format(type(ano)))
+print("O tipo da variável é {}".format(type(nome)))
+print("O tipo da variável é {}".format(type(saldo)))
