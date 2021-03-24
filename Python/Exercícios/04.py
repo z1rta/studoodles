d3ebbaac0465f5afcf1cfdc3c498c5dfa999af2c
Beatriz Marques Faria de Barros <71844667+z1rta@users.noreply.github.com>
2021-03-24 19:59:35 -0300

    Update 04.py

--- Python/Exercícios/04.py
+++ Python/Exercícios/04.py
@@ -1,6 +1,13 @@
+#descobrir tipos de variáveis
 ano = 1996
 nome = "Beatriz Marques"
 saldo = 56.99
 print("O tipo da variável é {}".format(type(ano)))
 print("O tipo da variável é {}".format(type(nome)))
 print("O tipo da variável é {}".format(type(saldo)))
+
+#conversão de um tipo de variável para outro:
+variavel_float = 7.3
+print(variavel_float)
+variavel_int = int(variavel_float)
+print(variavel_int)
