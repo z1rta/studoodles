4fd5cac0949c968061189c2695618b9f68c2363d
Beatriz Marques Faria de Barros <71844667+z1rta@users.noreply.github.com>
2021-04-03 22:26:11 -0300

    Create 20calc.py

---
+++ Python/Exercícios/20calc.py
@@ -0,0 +1,16 @@
+def somar(x, y):
+    return x + y
+
+
+def subtrair(x, y):
+    return x - y
+
+
+def multip(x, y):
+    return x * y
+
+
+def divi(x, y):
+    if b != 0:
+        return x / y
+    return 0
