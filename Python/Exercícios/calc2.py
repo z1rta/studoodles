516e1d8d54af17381d35dedb1286302b56bc72a5
Beatriz Marques Faria de Barros <71844667+z1rta@users.noreply.github.com>
2021-04-03 22:26:56 -0300

    Create calc2.py

---
+++ Python/Exercícios/calc2.py
@@ -0,0 +1,23 @@
+import calc
+
+opcao = -1
+
+while opcao != 5:
+    print('BEM VINDX A NOSSA CALCULADORA!')
+    print('~' * 30)
+    print('[1] - Soma\n[2] - Subtração\n[3] - Multiplicação\n[4] - Divisão\n[5] - Sair')
+    opcao = int(input('Digite qual operação deseja realizar: '))
+    print('~' * 30)
+
+    if opcao == 1:
+        print('~~~~~~~~~~~~ SOMA ~~~~~~~~~~~~')
+        print(calc.somar(float(input('Digite o primeiro valor: ')), float(input('Digite o segundo valor: '))))
+    elif opcao == 2:
+        print('~~~~~~~~~ SUBTRAÇÃO ~~~~~~~~~~')
+        print(calc.subtrair(float(input('Digite o primeiro valor: ')), float(input('Digite o segundo valor: '))))
+    elif opcao == 3:
+        print('~~~~~~~ MULTIPLICAÇÃO ~~~~~~~~')
+        print(calc.multip(float(input('Digite o primeiro valor: ')), float(input('Digite o segundo valor: '))))
+    elif opcao == 4:
+        print('~~~~~~~~~~ DIVISÃO ~~~~~~~~~~~')
+        print(calc.divi(float(input('Digite o primeiro valor: ')), float(input('Digite o segundo valor: '))))
